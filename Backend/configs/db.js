d81a047ca725f21364c585e5a40de7d2bb47a158
ThakoorRishwanth <rishijay13@gmail.com>
2024-08-03 17:01:30 +0530

    Backend

--- Backend/configs/db.js
+++ Backend/configs/db.js
@@ -1,7 +1,16 @@
-const {connect} = require('mongoose')
+const { connect } = require('mongoose');
 
-const connectToDb = async(uri)=>{
-    await connect(uri)
-}
+const connectToDb = async (uri) => {
+  try {
+    await connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
 
-module.exports = connectToDb
+module.exports = connectToDb;
